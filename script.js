7604bae5cbe1ef01bdce5fa4a6e14e8bbfb951ae
Denis <hallo-boy@mail.ru>
2022-09-07 09:14:48 +0300

    программа работает

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const check = document.querySelector('#check');
 const guests = document.querySelector('#guests');
 const quality = document.querySelector('#quality');
-const tipAmaunt = document.querySelector('#tip-amaunt');
+const tipAmount = document.querySelector('#tip-amaunt');
 
 
 calculate = () =>{
@@ -11,8 +11,16 @@ check.value = "";
 guests.value = "";
 quality.value = "";
 if(tip === "NaN"){
-    console.log("введите данные");
+    tipAmount.textContent = "введите данные";
+    showTipAmount();
 }else{
-    console.log("с каждого по " + tip + "рублей")
+    tipAmount.textContent = "с каждого по " + tip + "рублей";
+    showTipAmount();
 }
+};
+
+showTipAmount = () => {
+const a = document.querySelector('#tip-amaunt');
+a.className = "show";
+setTimeout(function(){ a.className = a.className.replace("show", ""); }, 3000);
 };
