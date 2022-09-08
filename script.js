1117ac04b48e349e5e2cb2f468aee47bbc37c0e6
Denis <hallo-boy@mail.ru>
2022-09-08 09:19:58 +0300

    comments

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
+//находим все поля ввода и блок в который будем добавлять итоговую сумму
+const check = document.querySelector("#check");
+const guests = document.querySelector("#guests");
+const quality = document.querySelector("#quality");
+const tipAmount = document.querySelector("#tip-amaunt");
 
-const check = document.querySelector('#check');
-const guests = document.querySelector('#guests');
-const quality = document.querySelector('#quality');
-const tipAmount = document.querySelector('#tip-amaunt');
-
-
-calculate = () =>{
-const tip = ((check.value*quality.value) / (guests.value)).toFixed(2);
-check.value = "";
-guests.value = "";
-quality.value = "";
-if(tip === "NaN"){
+//создаём функцию появления окна с выводом
+showTipAmount = () => {
+  //находим это окно
+  const a = document.querySelector("#tip-amaunt");
+  //меняем у него класс
+  a.className = "show";
+  //создаём функцию setTimeout, которая убирает класс спустя 3сек после его добавления
+  setTimeout(function () {
+    a.className = a.className.replace("show", "");
+  }, 3000);
+};
+//создаём функцию нахождения чаевых
+calculate = () => {
+  //находим чаевые используя значения введённые в инпуты
+  const tip = ((check.value * quality.value) / guests.value).toFixed(2);
+  //после нахождения очищаем поля ввода
+  check.value = "";
+  guests.value = "";
+  quality.value = "";
+  //создаём условную конструкцию в которой функцию появление итогового окна
+  if (tip === "NaN") {
     tipAmount.textContent = "введите данные";
     showTipAmount();
-}else{
-    tipAmount.textContent = "с каждого по " + tip + "рублей";
+  } else {
+    tipAmount.textContent = "с каждого по " + tip + "BYN";
     showTipAmount();
-}
-};
-
-showTipAmount = () => {
-const a = document.querySelector('#tip-amaunt');
-a.className = "show";
-setTimeout(function(){ a.className = a.className.replace("show", ""); }, 3000);
+  }
 };
